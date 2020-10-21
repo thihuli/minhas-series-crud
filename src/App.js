451fad6d39cd4d35f3233c57a499bac3af2989db
thihuli <thiagocandidof@hotmail.com>
2020-10-20 23:28:08 -0300

    alterado rotas

--- src/App.js
+++ src/App.js
@@ -5,24 +5,33 @@ import Header from './components/Header';
 import Genres from './components/Genres';
 import NewGenres from './components/NewGenres';
 import EditGenres from './components/EditGenres'
+import Series from './components/Series';
+import NewSeries from './components/NewSerie';
+import InforSerie from './components/InfoSerie';
 
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 
 
 function App() {
-  
-  
+
+
   return (
     <Router>
       <div>
         <Header />
-        <Route exact path='/'/>
-        <Route exact path='/generos/novo' component={NewGenres}/>
-        <Route exact path='/generos' component={Genres}/>
-        <Route exact path='/generos/:id' component={EditGenres}/>
+        <Switch>
+          <Route exact path='/' />
+          <Route exact path='/series' component={Series} />
+          <Route exact path='/generos' component={Genres} />
+          <Route exact path='/generos/novo' component={NewGenres} />
+          <Route exact path='/series/novo' component={NewSeries} />
+          <Route exact path='/generos/:id' component={EditGenres} />
+          <Route exact path='/series/:id' component={InforSerie} />
+        </Switch>
       </div>
     </Router>
   );
